44bb8d7650a94e1273509a3fef7dae4339b15e18
Michael Kanu <michaelokanu01@yahoo.com>
2024-08-29 22:30:02 +0000

    task2

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3
 """ regex function to obfuscate mesages
 """
-import re
+import logging
+from typing import List
 
 
-def filter_datum(fields, redaction, message, separator):
-    pattern = f"({'|'.join(fields)})=([^ {separator}]*)"
-    return re.sub(pattern, f"\\1={redaction}", message)
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
+        return super().format(record)
