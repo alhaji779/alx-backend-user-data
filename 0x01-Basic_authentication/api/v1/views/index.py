4cf7fc5c2283cff5bc2ed87961a8788b3d9cf327
Michael Kanu <michaelokanu01@yahoo.com>
2024-09-05 14:54:55 +0000

    401 error

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,11 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized/', strict_slashes=False)
+def unauthorised() -> None:
+    """ GET /api/v1/unauthorized
+    Returns: unauthorized response
+    """
+    abort(401)
