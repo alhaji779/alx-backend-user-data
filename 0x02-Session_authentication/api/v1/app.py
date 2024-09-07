2ff7c6038b11c89dee99d076470744c05f27af7b
Michael Kanu <michaelokanu01@yahoo.com>
2024-09-07 22:48:23 +0000

    session-expire

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -23,6 +23,9 @@ if a_type == 'basic_auth':
     auth = BasicAuth()
 if a_type == 'session_auth':
     auth = SessionAuth()
+if auth_type == 'session_exp_auth':
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
 
 
 @app.errorhandler(404)
