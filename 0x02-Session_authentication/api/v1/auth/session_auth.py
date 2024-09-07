29d0ccca0b7a2a0cc7c4547f79bd46242e14bf10
Michael Kanu <michaelokanu01@yahoo.com>
2024-09-07 21:55:05 +0000

    create=session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -11,3 +11,12 @@ from models.user import User
 class SessionAuth(Auth):
     """Session authentication class.
     """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a session id for the user.
+        """
+        if type(user_id) is str:
+            session_id = str(uuid4())
+            self.user_id_by_session_id[session_id] = user_id
+            return session_id
